b2c7b6b1e3cbf8b0db375b83f92c02e372b48ed4
Matthias <matt.bert@live.fr>
2024-01-14 14:48:49 +0100

    Start recording postgres database structure

---
+++ posgresql_database_creation.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE course_management;
+
+-- while using 'course_management' database
+
+CREATE TYPE roles AS enum('ADMINISTRATOR','TEACHER','STUDENT');
+
+CREATE TABLE Users (
+    id serial PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    role roles
+);
